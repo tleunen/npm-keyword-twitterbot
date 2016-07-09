1bd868129479c7d4d8c786076b6baa3f9965bfb2
Tommy Leunen <tommy.leunen+github@gmail.com>
2016-07-08 22:21:34 -0400

    feat(s3): Add support for Amazon S3
    BREAKING CHANGE: The option `cache_filepath` has been renamed in
    `local_cache_file`

--- README.md
+++ README.md
@@ -1,43 +1,57 @@
 # npm-keyword-twitterbot
-
-[![NPM](https://nodei.co/npm/npm-keyword-twitterbot.png)](https://nodei.co/npm/npm-keyword-twitterbot/)
+![npm][npm-version-image]
 
 This library allows you to automatically tweet new NPM modules based on keywords.
 
 ## Usage
 
-```
-var NPMTwitterBot = require('npm-keyword-twitterbot');
+```js
+require('npm-keyword-twitterbot')({
+    // Specify a local file to keep the cache.
+    local_cache_file: __dirname + '/cache_modules.json',
 
-new NPMTwitterBot({
-    cache_filepath: __dirname + '/cache_modules.json',
+    // S3 settings to save the file on your S3 bucket
+    s3_region: 'us-east-1',
+    s3_access_key: '',
+    s3_secret_access_key: '',
+    s3_bucket_name: '',
+    s3_file_key: 'npm-keyword-twitterbot/cache_modules.json',
 
-    consumer_key: 'consumer_key',
-    consumer_secret: 'consumer_secret',
-    access_token: 'access_token',
-    access_token_secret: 'access_token_secret',
+    // Twitter key/secret for the app
+    twitter_consumer_key: '',
+    twitter_consumer_secret: '',
+
+    // Twitter tokens for the user
+    twitter_access_token: '',
+    twitter_access_token_secret: '',
 
     npm_keyword: [
         'keyword1',
         'keyword2'
     ],
 
-    use_homepage_url: false, /* use homepage url set in package.json instead of npm url? */
+    // Enable this to use the homepage url set in package.json instead of the npm url
+    use_homepage_url: true,
 
-    tweet_hashtags: "#MyhashTag"
+    tweet_hashtags: ''
 });
 ```
 
 ## Example
 
-The twitter bot [ReactJSnpm](https://twitter.com/ReactJSnpm), based on the keyword `react` and `react-component`, tweets new and updated ReactJS modules.
+The [ReactJSnpm](https://twitter.com/ReactJSnpm) twitter bot is an example of bot using this script.
+
+Have a bot? Send a PR!
 
 ## How to use
 
 * Create a Twitter app for your bot: https://apps.twitter.com/
+* Create a specific Twitter user for your bot and retrieve its tokens.
 * Create a bot like the one in the `usage` section.
 * Add a cron job to start your bot. The cron job should run the script at least every 30 minutes.
 
 ## License
 
-MIT, see [LICENSE.md](http://github.com/tleunen/npm-keyword-twitterbot/blob/master/LICENSE.md) for details.
+MIT, see [LICENSE.md](/LICENSE.md) for details.
+
+[npm-version-image]: https://img.shields.io/npm/v/npm-keyword-twitterbot.svg
--- src/index.js
+++ src/index.js
@@ -3,16 +3,58 @@ const NPMTwit = require('./npmTwit');
 const semver = require('semver');
 const fs = require('fs');
 const defaults = require('lodash.defaults');
+const AWS = require('aws-sdk');
 
-function getCacheObject(filepath) {
-    if (fs.existsSync(filepath)) {
-        return JSON.parse(fs.readFileSync(filepath, 'utf8'));
+function getCacheObject(options, s3) {
+    if (s3) {
+        const params = {
+            Bucket: options.s3_bucket_name,
+            Key: options.s3_file_key
+        };
+
+        return s3.getObject(params).promise()
+        .then(response => JSON.parse(response.Body.toString('utf8')), err => {
+            if (err.code === 'NoSuchKey') {
+                return {};
+            }
+            throw err;
+        });
+    } else if (options.local_cache_file) {
+        if (fs.existsSync(options.local_cache_file)) {
+            return Promise.resolve(JSON.parse(fs.readFileSync(options.local_cache_file, 'utf8')));
+        }
+        return Promise.resolve({});
     }
-    return {};
+
+    throw new Error('`local_cache_file` or `s3_*` options are required.');
+}
+
+function saveCacheObject(cacheObject, options, s3) {
+    const cacheString = JSON.stringify(cacheObject);
+
+    if (s3) {
+        const params = {
+            Bucket: options.s3_bucket_name,
+            Key: options.s3_file_key,
+            Body: cacheString,
+            ContentType: 'application/json'
+        };
+
+        return s3.putObject(params).promise();
+    }
+
+    fs.writeFileSync(options.local_cache_file, cacheString, 'utf8');
+    return Promise.resolve();
 }
 
 module.exports = opts => {
     const options = defaults(opts, {
+        local_cache_file: '',
+        s3_region: '',
+        s3_access_key: '',
+        s3_secret_access_key: '',
+        s3_bucket_name: '',
+        s3_file_key: '',
         twitter_consumer_key: '',
         twitter_consumer_secret: '',
         twitter_access_token: '',
@@ -21,43 +63,55 @@ module.exports = opts => {
         use_homepage_url: false
     });
 
-    const cacheFilePath = options.cache_filepath || `${__dirname}/cache_modules.json`;
-    const cacheObject = getCacheObject(cacheFilePath);
+    let s3;
 
-    const npmTwit = new NPMTwit({
-        consumer_key: options.twitter_consumer_key,
-        consumer_secret: options.twitter_consumer_secret,
-        access_token: options.twitter_access_token,
-        access_token_secret: options.twitter_access_token_secret,
-        tweet_hashtags: options.tweet_hashtags,
-        use_homepage_url: options.use_homepage_url
-    });
+    const isAWSEnabled = Object.keys(options).filter(o => o.startsWith('s3_')).every(k => !!options[k]);
+    if (isAWSEnabled) {
+        s3 = new AWS.S3({
+            accessKeyId: options.s3_access_key,
+            secretAccessKey: options.s3_secret_access_key,
+            region: options.s3_region
+        });
+    }
 
-    npm.getModules(options.npm_keyword)
-    .then(npmModules => {
-        console.log(`modules: ${npmModules.length}`);
+    // get the cache
+    return getCacheObject(options, s3)
+    .then(cacheObject => {
+        const npmTwit = new NPMTwit({
+            consumer_key: options.twitter_consumer_key,
+            consumer_secret: options.twitter_consumer_secret,
+            access_token: options.twitter_access_token,
+            access_token_secret: options.twitter_access_token_secret,
+            tweet_hashtags: options.tweet_hashtags,
+            use_homepage_url: options.use_homepage_url
+        });
 
-        // retrieve only newer version of modules
-        const updatedModules = npmModules.filter(mod =>
-            !({}.hasOwnProperty.call(cacheObject, mod.name)) ||
-            semver.gt(mod.version, cacheObject[mod.name])
-        );
+        return npm.getModules(options.npm_keyword)
+        .then(npmModules => {
+            console.log(`modules: ${npmModules.length}`);
 
-        console.log(`updated modules: ${updatedModules.length}`);
+            // retrieve only newer version of modules
+            const updatedModules = npmModules.filter(mod =>
+                !({}.hasOwnProperty.call(cacheObject, mod.name)) ||
+                semver.gt(mod.version, cacheObject[mod.name])
+            );
 
-        if (true || updatedModules.length > 0) {
-            // tweet
-            npmTwit.tweetModules(updatedModules);
+            console.log(`updated modules: ${updatedModules.length}`);
 
-            // add the updated modules to the cache
-            const newCacheObj = updatedModules.reduce((cache, mod) => {
-                cache[mod.name] = mod.version; // eslint-disable-line no-param-reassign
-                return cache;
-            }, cacheObject);
+            if (updatedModules.length > 0) {
+                // tweet
+                npmTwit.tweetModules(updatedModules);
 
-            // save cache
-            fs.writeFileSync(cacheFilePath, JSON.stringify(newCacheObj), 'utf8');
-        }
+                // add the updated modules to the cache
+                const newCacheObj = updatedModules.reduce((cache, mod) => {
+                    cache[mod.name] = mod.version; // eslint-disable-line no-param-reassign
+                    return cache;
+                }, cacheObject);
+
+                return saveCacheObject(newCacheObj, options, s3);
+            }
+            return Promise.resolve();
+        });
     })
     .catch(err => console.error(err));
 };
